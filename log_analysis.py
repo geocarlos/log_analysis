a217ab78e152e4b5bf541683b13ff57b9a2945db
Geocarlos Lopes Alves <geowildcat@gmail.com>
2018-03-31 22:10:17 -0400

    Declare get_conn function for connection

--- log_analysis.py
+++ log_analysis.py
@@ -2,26 +2,27 @@ import psycopg2
 
 DBNAME = "news"
 
-def get_top_15():
-  """Initial function for testing. Return 20 logs for test."""
+def get_conn(query):
+  """Creates connection to be used in functions to retrieve data."""
   conn = psycopg2.connect(database=DBNAME)
   c = conn.cursor()
-  c.execute("select path, count(path) as num from log where path != '/' group by path order by num desc limit 15;")
-  info = c.fetchall()
+  c.execute(query)
+  data = c.fetchall()
   conn.close()
-  return info
+  return data
 
 def get_top_articles():
-    return "This should be the top three articles."
+    return get_conn('''select path, count(path) as num from log
+    where path != '/'
+    group by path
+    order by num desc
+    limit 3;''')
 
 def get_top_authors():
     return "This should be the top article authors"
 
 def get_error_days():
     return "This should be the days with more than 1% requests leading to error"
-
-for info in get_top_15():
-    print(info)
 
 print(get_top_articles())
 print(get_top_authors())
