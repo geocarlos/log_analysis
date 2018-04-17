d8914f85d1cac0682454794d7699c7456b047f8c
vagrant <vagrant@vagrant.vm>
2018-04-17 00:49:20 +0000

    Code function for finding top authors

--- log_analysis.py
+++ log_analysis.py
@@ -21,7 +21,7 @@ def get_top_articles():
     where path != '/' and a.slug = substr(l.path,10)
     group by a.title
     order by views desc
-    limit 8;''')
+    limit 3;''')
 
 """
 2. Who are the most popular article authors of all time?
@@ -29,10 +29,21 @@ That is, when you sum up all of the articles each author has written,
 which authors get the most page views?
 """
 def get_top_authors():
-    return get_conn('''select author, count(author) as num from articles
-    group by author
-    order by num desc
-    limit 10;''')
+    """
+    The query used here depends on a view,
+    which has been created with the code below:
+
+    create view authors_views as
+    select au.id, count(l.*) as views
+    from articles ar, authors au, log l where au.id = ar.author
+    and ar.slug =substr(l.path, 10)
+    group by au.id, l.path, ar.author;
+    """
+    return get_conn('''
+    select au.name, sum(av.views) as views
+    from authors au, authors_views av
+    where au.id = av.id group by av.id, au.name
+    order by views desc;''')
 
 """
 3. On which days did more than 1% of requests lead to errors?
@@ -44,7 +55,11 @@ def get_error_days():
     order by num desc
     limit 3;''')
 
-print("Top articles: ");
+print("\nTop articles: \n");
 for a in get_top_articles():
+    print(a[0] + " - " + str(a[1]) + " views")
+
+print("\n\nTop authors: \n");
+for a in get_top_authors():
     print(a[0] + " - " + str(a[1]) + " views")
 # print(get_error_days())
