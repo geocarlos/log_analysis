ef95b1c0f9131c836a6221f257d765d9ef5bd629
vagrant <vagrant@vagrant.vm>
2018-04-17 22:36:39 +0000

    Adjust code style

--- log_analysis.py
+++ log_analysis.py
@@ -2,19 +2,23 @@ import psycopg2
 
 DBNAME = "news"
 
+
 def get_conn(query):
-  """Creates connection to be used in functions to retrieve data."""
-  conn = psycopg2.connect(database=DBNAME)
-  c = conn.cursor()
-  c.execute(query)
-  data = c.fetchall()
-  conn.close()
-  return data
+    """Creates connection to be used in functions to retrieve data."""
+    conn = psycopg2.connect(database=DBNAME)
+    c = conn.cursor()
+    c.execute(query)
+    data = c.fetchall()
+    conn.close()
+    return data
+
 
 """
 1. What are the most popular three articles of all time?
 Which articles have been accessed the most?
 """
+
+
 def get_top_articles():
     return get_conn('''select a.title, count(a.title)
         as views from articles a, log l
@@ -23,11 +27,14 @@ def get_top_articles():
         order by views desc
         limit 3;''')
 
+
 """
 2. Who are the most popular article authors of all time?
 That is, when you sum up all of the articles each author has written,
 which authors get the most page views?
 """
+
+
 def get_top_authors():
     """
     The query used here depends on a view,
@@ -44,9 +51,12 @@ def get_top_authors():
         where au.id = av.id group by av.id, au.name
         order by views desc;''')
 
+
 """
 3. On which days did more than 1% of requests lead to errors?
 """
+
+
 def get_error_days():
     """
     [create view req_errors as
@@ -64,25 +74,28 @@ def get_error_days():
         where rt.day = re.day and re.errors > (rt.requests::real/100)
         group by rt.day, rt.requests, re.errors;''')
 
+
 """
 Make date human friendly
 """
+
+
 def formDate(date):
     months = {
-        '01':'January',
-        '02':'February',
-        '03':'March',
-        '04':'April',
-        '05':'May',
-        '06':'June',
-        '07':'July',
-        '08':'August',
-        '09':'September',
-        '10':'October',
-        '11':'November',
-        '12':'December'
-        }
+        '01': 'January',
+        '02': 'February',
+        '03': 'March',
+        '04': 'April',
+        '05': 'May',
+        '06': 'June',
+        '07': 'July',
+        '08': 'August',
+        '09': 'September',
+        '10': 'October',
+        '11': 'November',
+        '12': 'December'
+    }
     day = date[8:10]
     month = date[5:7]
     year = date[:4]
-    return months[month]+" "+day+", "+year
+    return months[month] + " " + day + ", " + year
